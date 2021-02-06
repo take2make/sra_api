31b5201bb960f5d0c757c35b8360f381b26a5cd0
Alexey Lyutikh <lutixalex1998@gmail.com>
2021-02-06 19:53:34 +0300

    reload scripts

--- scripts.py
+++ scripts.py
@@ -15,7 +15,7 @@ def delete_punctuation(txt_file):
 
 	output: текст без пунктуации в нижнем регистре
 	"""
-	
+
 	without_punctuation = re.sub(r'[^\w\s]','',txt_file)
 	without_numbers = re.sub(r'[0-9]+', '', without_punctuation)
 	return without_numbers.rstrip('\n').lower()
@@ -32,11 +32,11 @@ def decode_file(encoded_data, name, session, extension):
 		   extension - расширение декодированного файла
 	"""
 
-    file_res = f'{name}_{session}.{extension}'
-    with open(file_res, "wb") as file:
-        decoded_data = base64.b64decode(encoded_data)
-        file.write(decoded_data)
-    return file_res
+	file_res = f'{name}_{session}.{extension}'
+	with open(file_res, "wb") as file:
+		decoded_data = base64.b64decode(encoded_data)
+		file.write(decoded_data)
+	return file_res
 
 
 def convert_audio_to_mono_wav(decoded_data):
@@ -49,6 +49,6 @@ def convert_audio_to_mono_wav(decoded_data):
 	output: аудио данные в формате mono wav
 	"""
 
-    os.system(f'ffmpeg -i {decoded_data} -acodec pcm_s16le -ac 1 -ar 8000 {decoded_data}.wav')
-    os.remove(decoded_data)
-    return f'{decoded_data}.wav'
+	os.system(f'ffmpeg -i {decoded_data} -acodec pcm_s16le -ac 1 -ar 8000 {decoded_data}.wav')
+	os.remove(decoded_data)
+	return f'{decoded_data}.wav'
